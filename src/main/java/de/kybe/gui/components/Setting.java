0630cabbdddf2e46412d588d6a5207ccd816d06e
Kyrill Belomenov <apfel236@outlook.de>
2024-09-16 18:08:34 +0200

    license

--- src/main/java/de/kybe/gui/components/Setting.java
+++ src/main/java/de/kybe/gui/components/Setting.java
@@ -1,29 +1,39 @@
+/*
+ * Copyright (c) 2024 kybe236
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package de.kybe.gui.components;
 
+/*
+ * Base Settings class (Used for all settings)
+ */
 public class Setting {
 	private String name;
-	private float value;
-	private float min;
-	private float max;
+	private boolean shouldSerialize = true;
 
 	public Setting(String name, float value, float min, float max) {
 		this.name = name;
-		this.value = value;
-		this.min = min;
-		this.max = max;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public float getValue() {
-		return value;
+	public boolean shouldSerialize() {
+		return shouldSerialize;
 	}
 
-	public void setValue(float value) {
-		if (value >= min && value <= max) {
-			this.value = value;
-		}
+	public void serialize() {
+
+	}
+
+	public void deserialize() {
+		// Deserialize the setting
 	}
 }
