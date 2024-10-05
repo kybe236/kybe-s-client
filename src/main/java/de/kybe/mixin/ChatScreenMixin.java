4ca310c2980f09047ddd46fecb154a1454b199a5
Kyrill Belomenov <apfel236@outlook.de>
2024-10-05 14:22:06 +0200

    1.20.6

--- src/main/java/de/kybe/mixin/ChatScreenMixin.java
+++ src/main/java/de/kybe/mixin/ChatScreenMixin.java
@@ -7,7 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static de.kybe.Kybe.mc;
 
@@ -19,11 +18,11 @@ public abstract class ChatScreenMixin {
 			at = @At("HEAD"),
 			cancellable = true
 	)
-	private void onSendChatMessage(String s, boolean bl, CallbackInfoReturnable<Boolean> cir) {
+	private void onSendChatMessage(String s, boolean b, CallbackInfo ci) {
 		if (s.startsWith(Kybe.PREFIX) && s.length() > 1) {
 			mc.gui.getChat().addRecentChat(s);
 			CommandManager.executeCommand(s);
-			cir.setReturnValue(true);
+			ci.cancel();
 		}
 	}
 }
